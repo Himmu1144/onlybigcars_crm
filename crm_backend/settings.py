b335bfe43cb9065bcfd7957193b023ae36ca1fc5
TheSerpentCoder <himmu1144@gmail.com>
2025-01-29 16:49:24 +0530

    base url

--- crm_backend/settings.py
+++ crm_backend/settings.py
@@ -169,6 +169,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-BASE_URL = "http://34.131.203.17"
+BASE_URL = "http://34.131.86.189/"
 
 
